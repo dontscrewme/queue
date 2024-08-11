bbf2d1eaadd724b4eb1b4d1a02fc7c914860a041
dontscrewme <57172435+dontscrewme@users.noreply.github.com>
2024-08-11 21:35:16 +0800

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,10 +1,7 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
 
-#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 typedef struct queue_t queue_t;
 
@@ -12,10 +9,10 @@ queue_t *newQueue(unsigned size, unsigned elementSize);
 void deleteQueue(queue_t *me);
 int enqueue(queue_t *me, const void *item);
 int dequeue(queue_t *me, void *item);
-int sizeOfQueue(queue_t *me);
-int numOfElements(queue_t *me);
-int numOfEmptySlots(queue_t *me);
-queue_t *resizeQueue(queue_t *me, unsigned newSize);
+int getSize(queue_t *me, unsigned int *output);
+int getNumOfElements(queue_t *me, unsigned int *output);
+int getNumOfEmptySlots(queue_t *me, unsigned int *output);
+int resizeQueue(queue_t *me, unsigned newSize, queue_t **newQueue);
 
 extern int (*queue_error_callback)(const char *, ...);
 void queue_set_error_callback(int (*errback)(const char *, ...));
